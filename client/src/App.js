d9423209d5f889c7187ccdb1812f6db9e2a6d391
Nikita Luksha <nikita.luksha@gmail.com>
2018-07-16 21:15:04 +0300

    Fix router

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import PropTypes from 'prop-types';
 import {
-  BrowserRouter as Router,
+  Router,
   Route,
 } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
